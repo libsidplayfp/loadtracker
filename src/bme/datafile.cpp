d1f137d11ea2f6680858201dcf25e3eb0b193d85
Leandro Nini <drfiemost@users.sourceforge.net>
2026-07-07 20:44:15 +0200

    Update

--- src/bme/datafile.cpp
+++ src/bme/datafile.cpp
@@ -2,11 +2,11 @@
 // Datafile creator
 //
 
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
 
 #include "bme_end.h"
 
@@ -24,17 +24,13 @@ static HEADER header[MAXFILES];
 static char fullname[MAXFILES][MAXFILENAME];
 static int files;
 
-int main(int argc, char **argv);
-int addfile(HEADER *header, FILE *dest, char *name);
+bool addfile(HEADER *header, FILE *dest, char *name);
 void fwrite8(FILE *file, unsigned data);
 void fwritele16(FILE *file, unsigned data);
 void fwritele32(FILE *file, unsigned data);
 
 int main(int argc, char **argv)
 {
-    FILE *datafile, *listfile;
-    int c;
-
     if (argc < 3)
     {
         printf("Usage: DATAFILE <datafile> <filelistfile>\n\n"
@@ -46,36 +42,36 @@ int main(int argc, char **argv)
                "fonts.spr\n");
         return 0;
     }
-    listfile = fopen(argv[2], "rt");
+    FILE *listfile = std::fopen(argv[2], "rt");
     if (!listfile)
     {
         printf("ERROR: Couldn't open filelist.\n");
         return 1;
     }
-    datafile = fopen(argv[1], "wb");
+    FILE *datafile = std::fopen(argv[1], "wb");
     if (!datafile)
     {
         printf("ERROR: Couldn't create datafile.\n");
-        fclose(listfile);
+        std::fclose(listfile);
         return 1;
     }
-    memset(&header[0], 0, MAXFILES * sizeof(HEADER));
+    std::memset(&header[0], 0, MAXFILES * sizeof(HEADER));
     // Get names from list
     for (;;)
     {
         char searchname[64];
-        int d;
         FILE *test;
 
-        if (fscanf(listfile, "%63s", searchname) == EOF) break;
-        test = fopen(searchname, "rb");
+        if (std::fscanf(listfile, "%63s", searchname) == EOF) break;
+        test = std::fopen(searchname, "rb");
         if (test)
         {
-            fclose(test);
+            std::fclose(test);
 
-            strcpy(fullname[files], searchname);
+            std::strcpy(fullname[files], searchname);
 
-            for (c = strlen(fullname[files]) - 1; c >= 0; c--)
+            int c;
+            for (c = std::strlen(fullname[files]) - 1; c >= 0; c--)
             {
                 if (fullname[files][c] == '\\')
                 {
@@ -84,8 +80,8 @@ int main(int argc, char **argv)
                 }
             }
             if (c < 0) c = 0;
-            memset(header[files].name, 0, 13);
-            d = 0;
+            std::memset(header[files].name, 0, 13);
+            int d = 0;
             while (fullname[files][c])
             {
                 header[files].name[d] = toupper(fullname[files][c]);
@@ -98,69 +94,66 @@ int main(int argc, char **argv)
         if (files == MAXFILES) break;
     }
 
-    fclose(listfile);
+    std::fclose(listfile);
     // Write datafile header
-    fwrite("DAT!", 4, 1, datafile);
+    std::fwrite("DAT!", 4, 1, datafile);
     fwritele32(datafile, files);
     // Write incomplete fileheaders
-    for (c = 0; c < files; c++)
+    for (int c = 0; c < files; c++)
     {
         fwritele32(datafile, header[c].offset);
         fwritele32(datafile, header[c].length);
-        fwrite(header[c].name, 13, 1, datafile);
+        std::fwrite(header[c].name, 13, 1, datafile);
     }
     // Process each file
-    for (c = 0; c < files; c++)
+    for (int c = 0; c < files; c++)
     {
         printf("Adding %s...\n", header[c].name);
         if (!addfile(&header[c], datafile, fullname[c]))
         {
             printf("Terminating & deleting datafile...\n");
-            fclose(datafile);
+            std::fclose(datafile);
             remove(argv[1]);
             return 1;
         }
     }
     // Seek back to start & write correct headers
-    fseek(datafile, sizeof files + 4, SEEK_SET);
-    for (c = 0; c < files; c++)
+    std::fseek(datafile, sizeof files + 4, SEEK_SET);
+    for (int c = 0; c < files; c++)
     {
         fwritele32(datafile, header[c].offset);
         fwritele32(datafile, header[c].length);
-        fwrite(header[c].name, 13, 1, datafile);
+        std::fwrite(header[c].name, 13, 1, datafile);
     }
-    fclose(datafile);
+    std::fclose(datafile);
     printf("Everything OK!\n");
     return 0;
 }
 
-int addfile(HEADER *header, FILE *dest, char *name)
+bool addfile(HEADER *header, FILE *dest, char *name)
 {
-    FILE *src;
-    unsigned char *originalbuf;
-
-    src = fopen(name, "rb");
+    FILE *src = std::fopen(name, "rb");
     if (!src)
     {
         printf("ERROR: Couldn't open file %s\n", name);
-        return 0;
+        return false;
     }
-    header->offset = ftell(dest);
-    fseek(src, 0, SEEK_END);
-    header->length = ftell(src);
-    fseek(src, 0, SEEK_SET);
-    originalbuf = malloc(header->length);
+    header->offset = std::ftell(dest);
+    std::fseek(src, 0, SEEK_END);
+    header->length = std::ftell(src);
+    std::fseek(src, 0, SEEK_SET);
+    unsigned char *originalbuf = (unsigned char*)std::malloc(header->length);
     if (!originalbuf)
     {
         printf("ERROR: No memory to load file!\n");
         fclose(src);
-        return 0;
+        return false;
     }
     printf("* Loading file\n");
-    fread(originalbuf, header->length, 1, src);
-    fclose(src);
+    std::fread(originalbuf, header->length, 1, src);
+    std::fclose(src);
     printf("* Writing file (size was %d)\n", header->length);
-    fwrite(originalbuf, header->length, 1, dest);
-    free(originalbuf);
-    return 1;
+    std::fwrite(originalbuf, header->length, 1, dest);
+    std::free(originalbuf);
+    return true;
 }
